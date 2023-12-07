8237c81aaeb2ec44c75aee2ecfaec9acf47fbe9b
YanGuimaraesMartins <134331115+YanGuimaraesMartins@users.noreply.github.com>
2023-12-07 19:17:16 -0300

    Create ProviderEndereco.js

---
+++ src/entities/ProviderEndereco.js
@@ -0,0 +1,15 @@
+export class ProviderEndereco{
+  estado
+  cidade
+  bairro
+  rua
+  numero
+    
+    constructor(estado, cidade, bairro, rua, numero){
+        this.estado = estado
+        this.cidade = cidade
+        this.bairro = bairro
+        this.rua = rua
+        this.numero = numero
+    }
+}
