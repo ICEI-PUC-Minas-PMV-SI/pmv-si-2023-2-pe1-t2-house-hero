f4cbd08d7240e1016c618989c2649a0b717a2a5a
YanGuimaraesMartins <134331115+YanGuimaraesMartins@users.noreply.github.com>
2023-12-07 21:02:37 -0300

    Update ServiceProvider.js

--- src/entities/ServiceProvider.js
+++ src/entities/ServiceProvider.js
@@ -6,20 +6,13 @@ export class ServiceProvider{
     email
     password
     cpf
-    descricao
-    horario
-    endereco
-    contato
-
+    
     constructor(name, email, password, cpf){
         this.id = self.crypto.randomUUID()
         this.name = name
         this.email = email
         this.password = encrypt(password)
         this.cpf = cpf
-        this.descricao = descricao
-        this.horario = horario
-        this.endereco = endereco
-        this.contato = contato
+        
     }
 }
